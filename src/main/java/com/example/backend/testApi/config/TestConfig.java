974ef3f6228c68d3bd1a35df1be176ac60305088
Felipe Antunes <lipeanttuness@gmail.com>
2021-12-29 18:49:22 -0300

    Product

--- src/main/java/com/example/backend/testApi/config/TestConfig.java
+++ src/main/java/com/example/backend/testApi/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.backend.testApi.entities.Category;
 import com.example.backend.testApi.entities.Order;
+import com.example.backend.testApi.entities.Product;
 import com.example.backend.testApi.entities.User;
 import com.example.backend.testApi.entities.enums.OrderStatus;
 import com.example.backend.testApi.repositories.CategoryRepository;
 import com.example.backend.testApi.repositories.OrderRepository;
+import com.example.backend.testApi.repositories.ProductRepository;
 import com.example.backend.testApi.repositories.UserRepository;
 
 //classe usada para popular o nosso banco de dados utilizando objetos. //
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override // metodo usado para executar essa classe qnd o programa for iniciado. //
 	public void run(String... args) throws Exception {
@@ -41,7 +46,18 @@ public class TestConfig implements CommandLineRunner {
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		// produtos com suas devidas definiçoes. //
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+		// salvando as categorias e produtos criados no banco de dados. //
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		// usuarios / clientes. //
 		User u1 = new User(null, "Usuario1", "usuario1@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Usuario2", "usuario2@gmail.com", "977777777", "123456");
@@ -51,9 +67,9 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
-		// salvando os objetos criados no banco de dados. //
+		// salvando os usuarios e pedidos criados no banco de dados. //
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	}
 }
