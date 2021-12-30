3839173e93e94785a1f923118ff97350fca2caae
Felipe Antunes <lipeanttuness@gmail.com>
2021-12-30 14:45:14 -0300

    Exception handling - delete

--- src/main/java/com/example/backend/testApi/services/UserService.java
+++ src/main/java/com/example/backend/testApi/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.backend.testApi.entities.User;
 import com.example.backend.testApi.repositories.UserRepository;
+import com.example.backend.testApi.services.exceptions.DatabaseException;
 import com.example.backend.testApi.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+  // capturando a exceção e lancçando a minha exceção de serviço fazendo o tratamento do delete. //	
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
